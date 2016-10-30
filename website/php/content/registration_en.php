b8a3eef1a1a0456e188f2d0df0b5dad16de6c272
Sabine Zumstein [zes3] <sabizum@gmail.com>
2016-10-30 10:53:44 +0100

    Registration

--- website/php/content/registration_en.php
+++ website/php/content/registration_en.php
@@ -1,20 +1,25 @@
 <div class="content">
-    <?php include('registration_validation.php') ?>
-    <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+    <?php include('../validation/registration_validation.php') ?>
+    <form id='registration' method='post' accept-charset='UTF-8'>
         <fieldset>
-            <legend>Login</legend>
+            <legend>Registrierung</legend>
             <input type='hidden' name='submitted' id='submitted' value='1'/>
 
             <label for='username'>UserName:</label>
-            <input type='text' name='username' id='username' maxlength="50" value="<?php echo $name; ?>"/>
+            <input type='text' name='username' id='username' maxlength="50" value="<?php echo $username; ?>" required />
             <mark><?php echo $error_username; ?></mark>
             <br />
             <label for='email'>E-Mail:</label>
-            <input type='text' name='email' id='email' maxlength="50" value="<?php echo $email; ?>"/>
+            <input type='email' name='email' id='email' maxlength="50" value="<?php echo $email; ?>" required />
             <mark><?php echo $error_email; ?></mark>
             <br />
-            <label for='password'>Password:</label>
-            <input type='password' name='password' id='password' maxlength="50"/>
+            <label for='password'>Passwort:</label>
+            <input type='password' name='password' id='password' maxlength="50" required />
+            <mark><?php echo $error_password; ?></mark>
+            <br />
+            <label for='password_repeat'>Passwort wiederholen:</label>
+            <input type='password' name='password_repeat' id='password_repeat' maxlength="50" required />
+            <mark><?php echo $error_password_repeat; ?></mark>
             <br />
             <input type='submit' name='Submit' value='Submit'/>
         </fieldset>
--- website/php/validation/registration_validation.php
+++ website/php/validation/registration_validation.php
@@ -1,28 +1,44 @@
 <?php
 
 $success = true;
-$username = $email = $error_username = $error_password = $error_email = '';
-
-if (empty($_POST['username'])) {
-    $error_username = 'Bitte geben Sie einen Benutzernamen ein';
-    $success = false;
-}
-/*else if (($_POST['name']) exists in DB) {
+$username = $email = $error_username = $error_password = $error_password_repeat = $error_email = '';
+if(isset($_POST['Submit'])) {
+    if (empty($_POST['username'])) {
+        $error_username = 'Bitte geben Sie einen Benutzernamen ein';
+        $success = false;
+    } /*else if (($_POST['name']) exists in DB) {
     $error_username = 'Dieser Benutzername ist bereits vergeben, bitte geben Sie einen anderen ein';
 }*/
-else $name = $_POST['username'];
+    else {
+        $name = $_POST['username'];
+    }
 
-if (empty($_POST['password']) || !filter_var($_POST['password']))  {
-    $error_password = 'Sie haben nicht das richtige Passwort eingegeben. <a href="">Passwort vergessen?</a>';
-    $success = false;
-} else $password = $_POST['password'];
+    if (empty($_POST['password']) || !filter_var($_POST['password'])) {
+        $error_password = 'Sie haben nicht das richtige Passwort eingegeben. <a href="">Passwort vergessen?</a>';
+        $success = false;
+    } else {
+        $password = $_POST['password'];
+    }
 
-if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))  {
-    $error_email = 'Bitte geben Sie eine gültige E-Mail-Adresse an';
-    $success = false;
-} else $email = $_POST['email'];
+    if (empty($_POST['password_repeat']) || !filter_var($_POST['password_repeat']) || ($_POST['password_repeat'] !== $_POST['password'])) {
+        $error_password_repeat = 'Passwort stimmt nicht überein';
+        $success = false;
+    } else {
+        $password_repeat = $_POST['password_repeat'];
+    }
 
-if ($success) {
-    echo "<p>Sie haben sich erfolgreich registriert!</p>";
-    exit; }
+    if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $error_email = 'Bitte geben Sie eine gültige E-Mail-Adresse an';
+        $success = false;
+    } else {
+        $email = $_POST['email'];
+    }
+
+    if ($success) {
+        echo "<p>Sie haben sich erfolgreich registriert!</p>";
+        exit;
+    }
+}
+
+
 ?>
