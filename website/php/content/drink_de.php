f7138477b6056e8a21c5dea66cabd3ccc3be2e77
Sabine Zumstein [zes3] <sabizum@gmail.com>
2016-11-20 20:19:34 +0100

    Started Drink Detail View

--- website/php/content/drink_de.php
+++ website/php/content/drink_de.php
@@ -2,14 +2,43 @@
     if (! isset($_GET["id"])) {
         header("location: index.php?site=drinklist");
     }
+    $detail_drink_id = $_GET["id"];
+    $res = DbHelper::doQuery("select * from drink where id = $detail_drink_id;");
+    $detail_drink = $res->fetch_object("Drink");
+    $imageResult = DbHelper::doQuery("select path from Image where id = (select image_id from Images_for_Drink where drink_id = $detail_drink_id);");
+    $imagePath = $imageResult->fetch_assoc()["path"];
+    $detailIngredients = getDetailIngredientsFromDb($detail_drink_id);
 ?>
 <div class="leftBar">
-
+    <h5>Benötigte Zutaten:</h5>
+    <ul>
+    <?php
+        foreach ($detailIngredients as $ingredient) {
+            $ing_name = $ingredient->getName();
+            echo "<li>$ing_name</li>";
+        }
+    ?>
+    </ul>
 </div>
 <div class="content">
-    <h1>Drink!</h1>
-    <h3>Detailansicht eines Drinks</h3>
+    <h1><?php echo $detail_drink->getName(); ?></h1>
+    <div class="big_image">
+        <img src="../../pic/Drinks/<?php echo $imagePath; ?>" alt="<?php echo $detail_drink->getName(); ?>" />
+    </div>
+    <h3>Beschreibung</h3>
+    <div class="drink_description"><?php echo $detail_drink->getDescription(); ?></div>
 </div>
 <div class="rightBar">
-    
+    <h5>Andere neue Drinks:</h5>
 </div>
+
+<?php
+    function getDetailIngredientsFromDb($drink_id) {
+        $res = array();
+        $dbRes = DbHelper::doQuery("select * from Ingredient where id in (select ingredient_id from Ingredients_for_Drink where drink_id = $drink_id);");
+        while($ingredient = $dbRes->fetch_object("Ingredient")){
+            array_push($res, $ingredient);
+        }
+        return $res;
+    }
+?>
--- website/php/content/home_de.php
+++ website/php/content/home_de.php
@@ -1,12 +1,13 @@
 <?php
-    $res = DbHelper::doQuery("select * from drink where id = 3;");
+    $res = DbHelper::doQuery("select * from drink where id = 1;");
     $recent_drink = $res->fetch_object("Drink");
+    $recent_drink_id = $recent_drink->getId();
     $imageResult = DbHelper::doQuery("select path from Image where id = (select image_id from Images_for_Drink where drink_id = 1);");
     $imagePath = $imageResult->fetch_assoc()["path"];
 ?>
 <div class="leftBar">
     <h4>Neuster Drink:</h4>
-    <h3><?php echo $recent_drink->getName(); ?></h3>
+    <a href="index.php?site=drink&id=<?php echo $recent_drink_id; ?>"><h3><?php echo $recent_drink->getName(); ?></h3></a>
     <div class="side_image">
         <img src="../../pic/Drinks/<?php echo $imagePath; ?>" alt="<?php echo $recent_drink->getName(); ?>" />
     </div>
