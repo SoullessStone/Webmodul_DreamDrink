207dfb1f3c8be06ca2a3c06842b3bac3324ea0ae
SoullessStone <micheldc@windowslive.com>
2016-12-28 14:33:25 +0100

    Drinktable kapseln

---
+++ website/php/content/DrinklistTable.php
@@ -0,0 +1,56 @@
+<?php
+    class DrinklistTable {
+        private $id, $name, $description, $creator, $isPublic, $createdAt;
+        
+        public function render($model) {
+            ?>
+                Suche mit AJAX machen!
+                <table id="drinks">
+                    <tbody>
+                        <tr>
+                            <td><b>Name</b></td>
+                            <td><b>Beschreibung</b></td>
+                            <td><b>Ersteller</b></td>
+                            <td><b>Erstelldatum</b></td>
+                            <td><b>Bewertung</b></td>
+                        </tr>
+                        <?php
+                            $imglink = $_SESSION["baseURL"]."/pic/recommend_drink.png";
+                            foreach ($model->getAllDrinksFromDb() as $drink) {
+                                $drink_id = $drink->getId();
+                                $link = $_SESSION["baseURL"]."Drink?id=".$drink_id;
+                                $rateRes = DbHelper::doQuery("select * from Rating where drink_id = $drink_id");
+                                $ratingObj = $rateRes->fetch_object("Rating");
+                                $rateCountRes = DbHelper::doQuery("select COUNT(*) as rate_Count from Rating where drink_id = $drink_id");
+                                $rateCount = $rateCountRes->fetch_assoc()["rate_Count"];
+                                if (!empty($ratingObj)) {
+                                    $rating = $ratingObj->getRating();
+                                    $average_rate = $rating / $rateCount;
+                                }
+                                else
+                                    $average_rate = 'keine';
+
+                                echo "<tr>";
+                                echo "    <td><a href='$link'>".$drink->getName()."</a></td>";
+                                echo "    <td>".$drink->getDescription()."</td>";
+                                echo "    <td>".$drink->getCreator()."</td>";
+                                echo "    <td>".$drink->getCreatedAt()."</td>";
+                                echo "    <td>".$average_rate."<img src='$imglink' class='recommend_drink'/></td>";
+                                echo "</tr>";
+                            } 
+                        ?>
+                    </tbody>
+                </table>
+                <div id="modal" class="modal">
+                    <div class="modal-content">
+                        <span class="close">x</span>
+                        <h3>Empfehle diesen Drink einem Freund!</h3>
+                        <p>Einfach hier seine E-Mail-Adresse eintragen:</p>
+                        <input type="text">
+                        <p class="remark">Wir werden einzig diesen Drink verschicken, keinen Spam.</p>
+                    </div>
+                </div>
+            <?php 
+        }
+}
+?>
--- website/php/content/drinklist_de.php
+++ website/php/content/drinklist_de.php
@@ -1,3 +1,4 @@
+
 <div class="leftBar">
     <h4>Suche Drinks nach Inhalt:</h4>
 </div>
@@ -5,53 +6,10 @@
     <h1>Alle Drinks</h1>
     <p>Alle Drinks inkl. deiner Bewertung!</p>
 
-    Suche mit AJAX machen!
-    <table id="drinks">
-        <tbody>
-            <tr>
-                <td><b>Name</b></td>
-                <td><b>Beschreibung</b></td>
-                <td><b>Ersteller</b></td>
-                <td><b>Erstelldatum</b></td>
-                <td><b>Bewertung</b></td>
-            </tr>
-            <?php
-            $link = $_SESSION["baseURL"]."/pic/recommend_drink.png";
-                foreach ($this->model->getAllDrinksFromDb() as $drink) {
-                    $drink_id = $drink->getId();
-                    $link = $_SESSION["baseURL"]."Drink?id=".$drink_id;
-                    $rateRes = DbHelper::doQuery("select * from Rating where drink_id = $drink_id");
-                    $ratingObj = $rateRes->fetch_object("Rating");
-                    $rateCountRes = DbHelper::doQuery("select COUNT(*) as rate_Count from Rating where drink_id = $drink_id");
-                    $rateCount = $rateCountRes->fetch_assoc()["rate_Count"];
-                    if (!empty($ratingObj)) {
-                        $rating = $ratingObj->getRating();
-                        $average_rate = $rating / $rateCount;
-                    }
-                    else
-                        $average_rate = 'keine';
-
-                    echo "<tr>";
-                    echo "    <td><a href='$link'>".$drink->getName()."</a></td>";
-                    echo "    <td>".$drink->getDescription()."</td>";
-                    echo "    <td>".$drink->getCreator()."</td>";
-                    echo "    <td>".$drink->getCreatedAt()."</td>";
-                    echo "    <td>".$average_rate."<img src='<?php echo $link; ?>' class='recommend_drink'/></td>";
-                    echo "</tr>";
-                } 
-            ?>
-        </tbody>
-    </table>
-    <div id="modal" class="modal">
-        <div class="modal-content">
-            <span class="close">x</span>
-            <h3>Empfehle diesen Drink einem Freund!</h3>
-            <p>Einfach hier seine E-Mail-Adresse eintragen:</p>
-            <input type="text">
-            <p class="remark">Wir werden einzig diesen Drink verschicken, keinen Spam.</p>
-        </div>
-
-    </div>
+    <?php
+        $drinklistTable = new DrinklistTable();
+        $drinklistTable->render($this->model);
+    ?>
 </div>
 <div class="rightBar">
     <h4>Beliebteste Drinks</h4>
