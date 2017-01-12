48e44623b5b8bbe7daca23650af52e2688a69a60
Sabine Zumstein [zes3] <sabizum@gmail.com>
2017-01-12 17:07:10 +0100

    Registration

--- website/php/content/registration_de.php
+++ website/php/content/registration_de.php
@@ -13,50 +13,35 @@
     ?>
     <form id='login' name="regForm" action='' method='post' accept-charset='UTF-8'>
         <input type='hidden' name='submitted' id='submitted' value='1'/>
-        <div class="row">
-            <div class="col-left">
-                <label for='username'>Benutzername:</label>
-                <input type='text' name='username' id='username' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['username'] : ""; ?>"/>
-                <span id="usernameError" class="error">
-                    <?php
-                        if(isset($_GET['noUsername'])) echo 'Bitte Benutzernamen eingeben';
-                        if(isset($_GET['usernameAlreadyExists'])) echo 'Der gewählte Benutzername ist bereits vergeben, versuch es mit einem anderen.';
-                    ?>
-                </span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-left">
-                <label for='firstname'>Vorname:</label>
-                <input type='text' name='firstname' id='firstname' maxlength="30" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['firstname'] : ""; ?>"/>
-                <span id="firstnameError" class="error">
-                    <?php
-                        if(isset($_GET['noFirstname'])) echo '<br />Bitte Vornamen eingeben<br /><br />';
-                    ?>
-                </span>
-            </div>
-
-            <div class="col-right">
-                <label for='lastname'>Nachname:</label>
-                <input type='text' name='lastname' id='lastname' maxlength="45" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['lastname'] : ""; ?>"/>
-                <span id="lastnameError" class="error">
-                    <?php
-                        if(isset($_GET['noLastname'])) echo '<br />Bitte Nachnamen eingeben<br /><br />';
-                    ?>
-                </span>
-            </div>
-        </div>
-        <div class="row">
-        <div class="col-left">
-            <label for='email'>E-Mail:</label>
-            <input type='text' name='email' id='email' maxlength="254" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['email'] : ""; ?>"/>
-            <span id="emailError" class="error">
+        <p>
+            <label for='username'>Benutzername:</label>
+            <input type='text' name='username' id='username' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['username'] : ""; ?>"/>
+            <span id="usernameError" class="error">
                 <?php
-                if(isset($_GET['invalidMail'])) echo '<br />Bitte gültige Email-Adresse eingeben<br /><br />';
+                    if(isset($_GET['noUsername'])) echo 'Bitte Benutzernamen eingeben';
+                    if(isset($_GET['usernameAlreadyExists'])) echo 'Der gewählte Benutzername ist bereits vergeben, versuch es mit einem anderen.';
                 ?>
             </span>
-        </div>
-        <div class="col-right">
+        </p>
+        <p>
+            <label for='firstname'>Vorname:</label>
+            <input type='text' name='firstname' id='firstname' maxlength="30" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['firstname'] : ""; ?>"/>
+            <span id="firstnameError" class="error">
+                <?php
+                    if(isset($_GET['noFirstname'])) echo 'Bitte Vornamen eingeben';
+                ?>
+            </span>
+        </p>
+        <p>
+            <label for='lastname'>Nachname:</label>
+            <input type='text' name='lastname' id='lastname' maxlength="45" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['lastname'] : ""; ?>"/>
+            <span id="lastnameError" class="error">
+                <?php
+                    if(isset($_GET['noLastname'])) echo 'Bitte Nachnamen eingeben';
+                ?>
+            </span>
+        </p>
+        <p>
             <label for='homeNation'>Heimatland:</label>
             <select name="homeNation">
                 <option value="Schweiz" selected>Schweiz</option>
@@ -70,38 +55,42 @@
                     if(isset($_GET['noHomeNation'])) echo 'Bitte Land auswählen'; 
                 ?>
             </span>
-        </div>
-        </div>
-
-        <div class="row">
-            <div class="col-left">
+        </p>
+        <p>
+            <label for='email'>E-Mail:</label>
+            <input type='text' name='email' id='email' maxlength="254" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['email'] : ""; ?>"/>
+            <span id="emailError" class="error">
+                <?php
+                    if(isset($_GET['invalidMail'])) echo 'Bitte gültige Email-Adresse eingeben';
+                ?>
+            </span>
+        </p>
+        <p>
             <label for='password'>Passwort:</label>
             <input type='password' name='password' id='password' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['password'] : ""; ?>"/>
             <span id="passwordError" class="error">
                 <?php 
-                    if(isset($_GET['passwordsNotEqual'])) echo '<br />Die Passwörter stimmen nicht überein, bitte gib sie nochmals ein.';
-                    if(isset($_GET['passwordEmpty'])) echo '<br />Das Passwort darf nicht leer sein.';
+                    if(isset($_GET['passwordsNotEqual'])) echo 'Die Passwörter stimmen nicht überein, bitte gib sie nochmals ein.';
+                    if(isset($_GET['passwordEmpty'])) echo 'Das Passwort darf nicht leer sein.';
                 ?>
             </span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-left">
+        </p>
+        <p>
             <label for='passwordRepeat'>Passwort wiederholen:</label>
             <input type='password' name='passwordRepeat' id='passwordRepeat' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['passwordRepeat'] : ""; ?>"/>
             <span id="passwordError" class="error">
                 <?php 
-                    if(isset($_GET['passwordsNotEqual'])) echo '<br />Die Passwörter stimmen nicht überein, bitte gib sie nochmals ein.';
-                    if(isset($_GET['passwordRepeatEmpty'])) echo '<br />Das Passwort darf nicht leer sein.';
+                    if(isset($_GET['passwordsNotEqual'])) echo 'Die Passwörter stimmen nicht überein, bitte gib sie nochmals ein.';
+                    if(isset($_GET['passwordRepeatEmpty'])) echo 'Das Passwort darf nicht leer sein.';
                 ?>
             </span>
-            </div>
-        </div>
+        </p>
 
         <input type='submit' name='submit' value='Submit'/>
     </form>
     <br/>
- </div>
+    <a href="<?php echo $_SESSION['baseURL'].'Registration'; ?>">Noch kein Account? Hier registrieren!</a>
+</div>
 <div class="rightBar">
     <?php
         $drinkSearch = new DrinkSearch();
--- website/php/content/registration_en.php
+++ website/php/content/registration_en.php
@@ -2,106 +2,95 @@
     <?php
     ?>
 </div>
-<div id="wrapper" class="content registration">
+<div class="content">
     <h1>Registration</h1>
     <?php
         if (isset($_GET["error"])) {
     ?>
-        <span id="error" class="error">General error, retry later...</span>
+        <span id="error" class="error">A general error occurred, please try again later.</span>
     <?php
         }
     ?>
     <form id='login' name="regForm" action='' method='post' accept-charset='UTF-8'>
         <input type='hidden' name='submitted' id='submitted' value='1'/>
-        <div class="row">
-            <div class="col-left">
-                <label for='username'>Username:</label>
-                <input type='text' name='username' id='username' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['username'] : ""; ?>"/>
-                <span id="usernameError" class="error">
-                    <?php
-                        if(isset($_GET['noUsername'])) echo 'Type in username';
-                        if(isset($_GET['usernameAlreadyExists'])) echo 'Username is already taken. Chose another one.';
-                    ?>
-                </span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-left">
-                <label for='firstname'>Firstname:</label>
-                <input type='text' name='firstname' id='firstname' maxlength="30" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['firstname'] : ""; ?>"/>
-                <span id="firstnameError" class="error">
-                    <?php
-                        if(isset($_GET['noFirstname'])) echo '<br />Type in firstname<br /><br />';
-                    ?>
-                </span>
-            </div>
-
-            <div class="col-right">
-                <label for='lastname'>Lastname:</label>
-                <input type='text' name='lastname' id='lastname' maxlength="45" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['lastname'] : ""; ?>"/>
-                <span id="lastnameError" class="error">
-                    <?php
-                        if(isset($_GET['noLastname'])) echo '<br />Type in lastname<br /><br />';
-                    ?>
-                </span>
-            </div>
-        </div>
-        <div class="row">
-        <div class="col-left">
-            <label for='email'>Mail:</label>
-            <input type='text' name='email' id='email' maxlength="254" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['email'] : ""; ?>"/>
-            <span id="emailError" class="error">
-                <?php
-                if(isset($_GET['invalidMail'])) echo '<br />This was not a valid e-mail<br /><br />';
+        <p>
+            <label for='username'>User Name:</label>
+            <input type='text' name='username' id='username' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['username'] : ""; ?>"/>
+            <span id="usernameError" class="error">
+                <?php 
+                    if(isset($_GET['noUsername'])) echo 'Please insert a user name';
+                    if(isset($_GET['usernameAlreadyExists'])) echo 'The name is already in use, please choose another.';
                 ?>
             </span>
-        </div>
-        <div class="col-right">
-            <label for='homeNation'>Home state:</label>
+        </p>
+        <p>
+            <label for='firstname'>First Name:</label>
+            <input type='text' name='firstname' id='firstname' maxlength="30" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['firstname'] : ""; ?>"/>
+            <span id="firstnameError" class="error">
+                <?php 
+                    if(isset($_GET['noFirstname'])) echo 'Please insert your first name';
+                ?>
+            </span>
+        </p>
+        <p>
+            <label for='lastname'>Last Name:</label>
+            <input type='text' name='lastname' id='lastname' maxlength="45" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['lastname'] : ""; ?>"/>
+            <span id="lastnameError" class="error">
+                <?php 
+                    if(isset($_GET['noLastname'])) echo 'Please insert your last name';
+                ?>
+            </span>
+        </p>
+        <p>
+            <label for='homeNation'>Home Country:</label>
             <select name="homeNation">
-                <option value="Schweiz" selected>Schweiz</option>
-                <option value="Deutschland">Deutschland</option>
-                <option value="Frankreich">Frankreich</option>
+                <option value="Schweiz" selected>Switzerland</option>
+                <option value="Deutschland">Germany</option>
+                <option value="Frankreich">France</option>
                 <option value="USA">USA</option>
-                <option value="Sonstwo">Sonstwo</option>
+                <option value="Sonstwo">Somewhere else</option>
             </select>
             <span id="homeNationError" class="error">
                 <?php 
-                    if(isset($_GET['noHomeNation'])) echo 'Chose state...'; 
+                    if(isset($_GET['noHomeNation'])) echo 'Please choose your home country';
                 ?>
             </span>
-        </div>
-        </div>
-
-        <div class="row">
-            <div class="col-left">
+        </p>
+        <p>
+            <label for='email'>E-Mail:</label>
+            <input type='text' name='email' id='email' maxlength="254" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['email'] : ""; ?>"/>
+            <span id="emailError" class="error">
+                <?php 
+                    if(isset($_GET['invalidMail'])) echo 'Please insert a valid E-Mail Address';
+                ?>
+            </span>
+        </p>
+        <p>
             <label for='password'>Password:</label>
             <input type='password' name='password' id='password' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['password'] : ""; ?>"/>
             <span id="passwordError" class="error">
                 <?php 
-                    if(isset($_GET['passwordsNotEqual'])) echo '<br />Passwords did not match';
-                    if(isset($_GET['passwordEmpty'])) echo '<br />Password must not be empty';
+                    if(isset($_GET['passwordsNotEqual'])) echo "The passwords didn't match, please try again.";
+                    if(isset($_GET['passwordEmpty'])) echo "Please insert a password.";
                 ?>
             </span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-left">
-            <label for='passwordRepeat'>Repeat password:</label>
+        </p>
+        <p>
+            <label for='passwordRepeat'>Password repeat:</label>
             <input type='password' name='passwordRepeat' id='passwordRepeat' maxlength="50" value="<?php echo isset($_SESSION['registration_data']) ? $_SESSION['registration_data']['passwordRepeat'] : ""; ?>"/>
             <span id="passwordError" class="error">
                 <?php 
-                    if(isset($_GET['passwordsNotEqual'])) echo '<br />Passwords did not match';
-                    if(isset($_GET['passwordRepeatEmpty'])) echo '<br />Password must not be empty';
+                    if(isset($_GET['passwordsNotEqual'])) echo "The passwords didn't match, please try again.";
+                    if(isset($_GET['passwordRepeatEmpty'])) echo "Please insert a password.";
                 ?>
             </span>
-            </div>
-        </div>
+        </p>
 
         <input type='submit' name='submit' value='Submit'/>
     </form>
     <br/>
- </div>
+    <a href="<?php echo $_SESSION['baseURL'].'Registration'; ?>">No account yet? Register here!</a>
+</div>
 <div class="rightBar">
     <?php
         $drinkSearch = new DrinkSearch();
